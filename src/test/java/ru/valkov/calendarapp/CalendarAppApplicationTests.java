0e7c2407d56849ea98ec0cd1e7fd58b34f1e36fc
dvalkoff <dmivalkov@gmail.com>
2022-05-02 15:35:07 +0300

    configure spring and maven

---
+++ src/test/java/ru/valkov/calendarapp/CalendarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.valkov.calendarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
